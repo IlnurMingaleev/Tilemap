2aa74301515c58662425ad74fa47346b258bddcc
Ilnur-web <ilmingaleev@yandex.ru>
2022-07-06 20:17:02 +0300

    Added Hero

---
+++ Assets/Scripts/IsometricPlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricPlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    [SerializeField] private float _speed;
+    [SerializeField] private Joystick joystick;
+    private PlayerAnimation animation;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        animation = GetComponent<PlayerAnimation>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector2 currentPos = rigidBody2D.position;
+        
+        float horizontalMove = JoystickInput(joystick.Horizontal);
+        float verticalMove = JoystickInput(joystick.Vertical);
+
+        Vector2 moveVector = new Vector2(horizontalMove, verticalMove);
+        moveVector = Vector2.ClampMagnitude(moveVector, 1);
+        animation.SetDirection(moveVector);
+
+        Vector2 newPos = currentPos + moveVector;
+        rigidBody2D.MovePosition(newPos);
+    }
+    private float JoystickInput(float axis) 
+    {
+        if (axis > .2f)
+        {
+            return _speed;
+        }
+        else if (axis < -.2f)
+        {
+            return -_speed;
+        }
+        else 
+        {
+            return 0;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private string[] IdleDirections = { "Idle_N", "Idle_NW", "Idle_W", "Idle_SW", "Idle_S", "Idle_SE", "Idle_E", "Idle_NE" };
+    private string[] WalkDirections = { "Walk_N", "Walk_NW", "Walk_W", "Walk_SW", "Walk_S", "Walk_SE", "Walk_E", "Walk_NE" };
+
+    private Animator animator;
+    private int lastDirection;
+    private // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetDirection(Vector2 direction) 
+    {
+        string[] directionArray = null;
+
+
+        if (direction.magnitude < .01f) //Character is static.
+        {
+            directionArray = IdleDirections;
+        }
+        else 
+        {
+            directionArray = WalkDirections;
+
+            lastDirection = DirectionIndex(direction); // Get the index of the slcie from the direction vector
+        }
+        animator.Play(directionArray[lastDirection]);
+    }
+
+    // Converts Vector2 direction to an index which we use to detemine animation.
+    private int DirectionIndex(Vector2 direction)
+    { 
+        //Returns this vector with a magnitude of 1 (Read Only).
+        //When normalized, a vector keeps the same direction but its length is 1.0.
+
+        Vector2 normalizedDirection = direction.normalized;
+        
+        // We are dividing to 8 because we have 8 directions in compass
+
+        float step = 360 / 8;
+        // By this calculation we achieve offset from each side of compass direction
+
+        float offset = step / 2;
+
+        //Angle between vectors
+
+        float angle = Vector2.SignedAngle(Vector2.up, normalizedDirection);
+
+        angle += offset;
+
+        //Here we avoid using negative angles
+
+        if (angle < 0)
+        {
+            angle += 360;
+        }
+
+        float stepCount = angle / step;
+
+        //Returns the largest integer smaller to or equal to
+
+        return Mathf.FloorToInt(stepCount);
+
+    }
+}
